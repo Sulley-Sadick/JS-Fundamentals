0652f7edbab847a92434644cab282a6871379106
Sulley Sadick <sulleysadick766@gmail.com>
2025-11-08 03:24:52 +0000

    Changed the logging of message to the console

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,4 +1,6 @@
 const c = "C is fun";
 const python = "Python is cool";
 const javascript = "JavaScript is amazing";
-console.log(c, python, javascript);
+console.log(c);
+console.log(python);
+console.log(javascript);
