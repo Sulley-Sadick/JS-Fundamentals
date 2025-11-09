4e616e0aab090f4873fee05d6d5482ee7567b12e
Sulley Sadick <sulleysadick766@gmail.com>
2025-11-09 15:34:11 +0000

    committed the 2-arguments.js file

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,5 +1,9 @@
 const argv = process.argv.slice(2);
 
-if (argv.length === 0) console.log("No argument");
-else if (argv.length === 1) console.log("Argument found");
-else console.log("Arguments found");
+if (argv.length === 0) {
+  console.log("No argument");
+} else if (argv.length === 1) {
+  console.log("Argument found");
+} else if (argv.length >= 2) {
+  console.log("Arguments found");
+}
