eb1f79e37eff8d6fa575bfae1fcba06f98483cb8
Sulley Sadick <sulleysadick766@gmail.com>
2025-11-09 01:49:49 +0000

    Created function with no arguments

--- 2-arguments.js
+++ 2-arguments.js
@@ -4,30 +4,3 @@ const noArguments = function () {
 };
 
 noArguments();
-
-// Function with one arguments
-const oneArguments = function (arg) {
-  console.log("Arguments found");
-  console.log(
-    `This is the arguments being passed as a value to this function: ${arg}`
-  );
-};
-
-oneArguments("One arguments");
-
-// Function with three arguments
-const arr = ["Mon", "Tue", "Wed"];
-const threeArguments = function (...days) {
-  console.log("Arguments found");
-
-  // looping over the arr array
-  arr.forEach((day, index) =>
-    console.log(
-      `We have ${days.length} serious days of every week, day ${
-        index + 1
-      }: ${day}`
-    )
-  );
-};
-
-threeArguments(...arr);
