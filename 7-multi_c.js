c75601dc7c59ed75998adb67863aa5a36d5c2bef
Sulley Sadick <sulleysadick766@gmail.com>
2025-11-10 12:34:18 +0000

    made some few tweaks with the 7-multi_cs.js code

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -2,4 +2,4 @@ const argv5 = process.argv.slice(2);
 
 if (isNaN(argv5[0])) console.log("Missing number of occurrences");
 if (argv5[0] < 0) return;
-else argv5.forEach((value) => console.log(`C is fun\n`.repeat(value)));
+else console.log(`C is fun\n`.repeat(argv5[0]));
